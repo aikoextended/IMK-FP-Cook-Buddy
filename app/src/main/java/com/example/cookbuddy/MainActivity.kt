67e00fdfba484f9a1388ed47ae7b80b45a90df3d
unknown <allenkeyo0405@gmail.com>
2025-05-15 06:15:29 +0700

    first commit

---
+++ app/src/main/java/com/example/cookbuddy/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.cookbuddy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cookbuddy.ui.theme.CookBuddyTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            landing()
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/cookbuddy/landing.kt
@@ -0,0 +1,106 @@
+package com.example.cookbuddy
+
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun landing(){
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color(0xFF4F070D)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logofix),
+            contentDescription = "Logo",
+            modifier = Modifier.size(200.dp))
+        Spacer(
+            modifier = Modifier.height(25.dp)
+        )
+        Text(
+            text = "Say Hello to",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Spacer(
+            modifier = Modifier.height(2.dp)
+        )
+        Text(
+            text = "Cook Buddy",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Spacer(
+            modifier = Modifier.height(18.dp)
+        )
+        Text(
+            text = "Your personal cooking assistant with voice recognition for easy and hands-free cooking.",
+            color = Color.White,
+            modifier = Modifier.width(300.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 14.sp,
+            lineHeight = 20.sp
+        )
+        Spacer(
+            modifier = Modifier.height(160.dp)
+        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xFF4F070D),
+                containerColor = Color.White
+            ),
+            modifier = Modifier.width(350.dp).height(45.dp)
+        ) {
+            Text(
+                text = "Let's Get Started",
+                fontSize = 16.sp
+            )
+        }
+        Spacer(
+            modifier = Modifier.height(16.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Already have an account? ",
+                color = Color.White,
+                fontSize = 16.sp
+            )
+            Text(
+                text = "Log in!",
+                color = Color.White,
+                modifier = Modifier.clickable {  },
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
